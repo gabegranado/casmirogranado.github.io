1096244f9bc9b3094d0fbb1d893f6f8331b5e239
Gabe Granado <gabegranado@Gabes-MacBook-Pro.local>
2024-01-29 08:18:59 -0600

    updated website

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -8,6 +8,7 @@ const endpoints = {
   education: 'profile/education.json',
   experiences: 'profile/experiences.json',
   projects: 'profile/projects.json',
+  dog: 'profile/dog.json',
 };
 
 export default endpoints;
